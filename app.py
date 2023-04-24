076a870ab1d1c2d2eaafcbd9a929016f1e6abb48
Austin Do <austinndo@gmail.com>
2023-04-24 14:55:29 -0700

    Completed task 2, import pandas, read data, and display data table

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from dash import Dash, html
+# Importing packages
+from dash import Dash, html, dash_table
+import pandas as pd
 
+# Adding data
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+
+# Initialize app
 app = Dash(__name__)
 
+# App layout
 app.layout = html.Div([
-    html.Div(children='Hello World')
+    html.Div(children='My first App with Data'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
 ])
 
+# To run the app:
 if __name__ == '__main__':
     app.run_server(debug=True)
