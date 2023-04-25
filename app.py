731469b0130f2961dbdcf91b00ba6a8863046cc4
Austin Do <austinndo@gmail.com>
2023-04-25 10:27:52 -0700

    Successfully rendered histogram of data

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Importing packages
-from dash import Dash, html, dash_table
+from dash import Dash, html, dash_table, dcc
 import pandas as pd
+import plotly.express as px
 
 # Adding data
 df = pd.read_csv(
@@ -12,7 +13,9 @@ app = Dash(__name__)
 # App layout
 app.layout = html.Div([
     html.Div(children='My first App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
+    dash_table.DataTable(data=df.to_dict('records'), page_size=10),
+    dcc.Graph(figure=px.histogram(
+        df, x='continent', y='lifeExp', histfunc='avg'))
 ])
 
 # To run the app:
